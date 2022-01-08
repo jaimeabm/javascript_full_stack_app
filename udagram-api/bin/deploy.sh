6b3b350e60b142afe55f0c34b4ee9652de7b910a
Jaime A Banda <jaime.banda@outlook.com>
2022-01-08 01:05:56 -0700

    update

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,3 +1,7 @@
-eb init --region us-east-1 --platform node.js-12 udagram
-eb use Udagram-env
-eb deploy Udagram-env
+#eb init --region us-east-1 --platform node.js-12 udagram
+#eb use Udagram-env
+#eb deploy
+
+aws s3 cp ./www/Archive.zip s3://elasticbeanstalk-us-east-1-757296424427/deploy.zip
+aws elasticbeanstalk create-application-version --application-name udagram --version-label <<pipeline.git.revision>> --source-bundle S3Bucket="elasticbeanstalk-us-east-1-757296424427",S3Key="Archive.zip"
+aws elasticbeanstalk update-environment --application-name udagram --environment-name Udagram-env --version-label <<pipeline.git.revision>>
