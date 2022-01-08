534d791fbf02aa8906aca8c655032b0b32ad9098
Jaime A Banda <jaime.banda@outlook.com>
2022-01-08 11:52:40 -0700

    update

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,5 @@
-aws s3 cp ./www/Archive.zip s3://elasticbeanstalk-us-east-1-757296424427
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_DEFAULT_REGION AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_SECRET=$AWS_SECRET JWT_SECRET=$JWT_SECRET PORT=$PORT POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD DB_PORT=$DB_PORT POSTGRES_USERNAME=$POSTGRES_USERNAME URL=$URL
-aws elasticbeanstalk create-application-version --application-name udagram --version-label v1 --source-bundle S3Bucket="elasticbeanstalk-us-east-1-757296424427",S3Key="Archive.zip"
-aws elasticbeanstalk update-environment --application-name udagram --environment-name Udagram-env --version-label v1
+eb deploy
+#aws s3 cp ./www/Archive.zip s3://elasticbeanstalk-us-east-1-757296424427
+#aws elasticbeanstalk create-application-version --application-name udagram --version-label v1 --source-bundle S3Bucket="elasticbeanstalk-us-east-1-757296424427",S3Key="Archive.zip"
+#aws elasticbeanstalk update-environment --application-name udagram --environment-name Udagram-env --version-label v1
